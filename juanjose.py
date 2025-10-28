11533034e38de8667933019964d99955253a0c7d
juanes <jecg050505@gmail.com>
2025-10-28 11:30:58 -0500

    first commit

---
+++ juanjose.py
@@ -0,0 +1,8 @@
+name= input("Ingresa tu nombre: ")
+age= input("Ingresa tu edad: ")
+
+
+if age >="18":
+    print(f"Mi nombre es: {name} y tengo {age} años y soy mayor de edad")
+else:
+    print(f"Mi nombre es: {name} y tengo {age} años y soy menor de edad")
